fbec2fdc735257535ffeecb61c562d5ab5fc1430
[zhaochaochao] <[zhaochaochao@baidu.com]>
2021-01-02 17:23:49 +0800

    Little change

--- syncConcurrent/futureAsync.cpp
+++ syncConcurrent/futureAsync.cpp
@@ -19,8 +19,8 @@ int main() {
     // 如果wait_for，会使得future async被执行，这里status为std::future_status::ready，相应的下一行代码会输出str转换后的值
     std::cout << "status: " << (int) res.wait_for(std::chrono::milliseconds(100)) << std::endl;
 
-    std::cout << str << std::endl; // 这时异步动作没跑，所以str为原来的值
-    std::cout << res.get() << "--[" << std::this_thread::get_id() << "]--" << str << std::endl; // get动作使得当前线程等待异步动作结束，所以str值被修改
+    std::cout << str << std::endl; // 这时异步动作已完成，str为修改后的值
+    std::cout << res.get() << "--[" << std::this_thread::get_id() << "]--" << str << std::endl;
 
     std::cout << std::endl;
 
@@ -29,7 +29,7 @@ int main() {
     // 创建异步动作，policy为launch::deferred，deferred代表func在显示调用get或wait时才会跑，但是都是在同一个线程跑，如thread::get_id打印
     std::future<int> res2 = std::async(std::launch::deferred, func, std::ref(str2));
 
-    // 如果wait_for，future async因为是在同一个线程执行，所以无法被执行，这里status为std::future_status::deferred
+    // 如果wait_for，future async因为是在同一个线程执行，所以无法被执行
     // std::cout << "status: " << (int) res.wait_for(std::chrono::milliseconds(100)) << std::endl;
 
     std::cout << str2 << std::endl; // 这时异步动作没跑，所以str为原来的值
